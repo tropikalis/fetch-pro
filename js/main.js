a893f0980ef446ecfb7f8ac14f281b45d3bbd306
tropikais <desertukas@yahoo.com>
2024-09-11 09:01:58 +0300

    fetch-github-link

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@
 
 const ulDOM = document.querySelector('ul');
 
-const data = [];
+const data = fetch('https://tropikalis.github.io/fetch-pro/data/users.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        for (const user of data) {
+            console.log(user);
+        }
+    });
 
-let HTML = '';
-
-for (const item of data) {
-    HTML += `<li>${item.id}) ${item.text}</li>`;
-}
-
-ulDOM.innerHTML = HTML;
+// let HTML = '';
+// 
+// for (const item of data) {
+//     HTML += `<li>${item.id}) ${item.text}</li>`;
+// }
+// 
+// ulDOM.innerHTML = HTML;
